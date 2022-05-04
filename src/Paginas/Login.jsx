c1b923d4b5806d09ed3fb096e54988ad6e504065
JoelMax13 <joel.ona@hotmail.com>
2022-05-03 23:30:04 -0500

    0.5
    Ingreso de Usuarios al sistema

--- src/Paginas/Login.jsx
+++ src/Paginas/Login.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import '../Css-Componentes/Login.css';
 import '../Css-Componentes/FondoBase.css';
@@ -8,105 +9,93 @@ import axios from "axios";
 
 const uri = "https://doctorfastapi.herokuapp.com/login";
 
-class Login extends Component{
-    peticionLogin(usuario, contrasena) {
+export default function Login(){
+
+    const [usu, setUsu] = useState("");
+    const [con, setCon] = useState("");
+
+    function inicioSesion(e){
+        e.preventDefault();
+        let txtUsu = document.getElementById("txtUsuario").value;
+        let txtCon = document.getElementById("txtContrasena").value;
+        document.getElementById("txtUsuario").value = "";
+        document.getElementById("txtContrasena").value = "";
+        peticionLogin(txtUsu, txtCon);
+    }
+
+    const navigate = useNavigate();
+
+    function peticionLogin(usuario, contrasena) {
         //username: 'Hernesto34' , password: '12345678'
         const body = { usuario: usuario , contrasena: contrasena };
         axios.post(uri, body)
             .then(response => {if (response.data.jwt) {
                 let auth = response.data.auth;
                 let rol = response.data.rol;
-                localStorage.setItem("user", JSON.stringify(response.data));
                 if(auth === true){
+                    localStorage.setItem("user", JSON.stringify(response.data));
                     if(rol === 1){
-                        console.log("Pagina Paciente");
+                        navigate('/iniciopaciente');
                     }else{
-                        console.log("Pagina Doctor");
+                        // localStorage.getItem('user');
+                        navigate('/iniciodoctor');
                     }
                 }else{
                     alert("Usuario o contraseña incorrectos. Intente de nuevo.");
                 }
             }else{
-                console.log("hola");
+                alert("hola");
             } 
             })
-            // return response.data;})
-            .catch(error => console.log(error));
-
-        // axios.post(uri, body)
-        //     .then(response => console.log(response.data))
-        //     .catch(error => console.log(error));
+            .catch(error => alert(error));
     }
 
-    state = {
-        esVistaContrasena : false
-    }
-    cambiarVisibilidadContrasena =()=>{
-        const {esVistaContrasena} = this.state;
-        this.setState({esVistaContrasena : !esVistaContrasena});
-    }
-
-    usuario = React.createRef();
-    contrasena = React.createRef();
-
-    obtenerDatos = (event) => {
-        event.preventDefault();
-        let usuarioP = this.usuario.current.value;
-        let contrasenaP = this.contrasena.current.value;
-        // console.log(usuarioP, contrasenaP);
-        this.peticionLogin(usuarioP, contrasenaP);
-    }
-
-    render(){
-        const {esVistaContrasena} = this.state;
         return(
-            // <form onSubmit={this.peticionLogin}>
-            // <form>
-            <form onSubmit={this.obtenerDatos}>
-                <div className='fondo'>
-                    <div className='cuadro'>
-                        <h1 className='h1'>Ingreso:</h1>
-                        <div className="container">
-                            <div className="mb-3 row">
-                                <label className="col-sm-3 col-form-label lblArriba">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                                    <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/></svg>
-                                    Número de identidad:
-                                </label>
-                                <div className="col-sm-9 txtAbajo">
-                                    <input ref={this.usuario} type='text' name="usuario" className="form-control" id="TxtCorreo" placeholder=""/>
-                                </div>
+        // <form onSubmit={this.peticionLogin}>
+        // <form>
+        <form onSubmit={inicioSesion}>
+            <div className='fondo'>
+                <div className='cuadro'>
+                    <h1 className='h1'>Ingreso:</h1>
+                    <div className="container">
+                        <div className="mb-3 row">
+                            <label className="col-sm-3 col-form-label lblArriba">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/></svg>
+                                Número de identidad:
+                            </label>
+                            <div className="col-sm-9 txtAbajo">
+                                <input id='txtUsuario' type='text' className="form-control" onChange={(e)=>setUsu(e.target.value)} required/>
                             </div>
-                            <div className="mb-3 row">
-                                <label className="col-sm-3 col-form-label lblArriba">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-key-fill" viewBox="0 0 16 16">
-                                        <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
-                                    </svg>
-                                     Contraseña:
-                                </label>
-                                <div className="col-sm-9 txtAbajo">
-                                    <input ref={this.contrasena} type={(esVistaContrasena) ? "text" : "password"} name="contrasena" className="form-control" id="TxtContrasena"/>
-                                </div>
+                        </div>
+                        <div className="mb-3 row">
+                            <label className="col-sm-3 col-form-label lblArriba">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-key-fill" viewBox="0 0 16 16">
+                                    <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+                                </svg>
+                                 Contraseña:
+                            </label>
+                            <div className="col-sm-9 txtAbajo">
+                                <input id='txtContrasena' type='password' className="form-control" onChange={(e)=>setCon(e.target.value)} required/>
                             </div>
-                            <div>
-                                <button id="BtnVerContrasena" type="button" onClick={this.cambiarVisibilidadContrasena}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye-fill" viewBox="0 0 16 16">
-                                        <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
-                                        <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
-                                    </svg>
-                                </button>
-                                <label htmlFor="BtnVerContrasena">Mostrar Contraseña</label>
-                            </div>
-                            <button className="btn btn-primary" id="BtnIngresar" type='submit'>Ingresar</button>
-                            <div className="abajo">
-                                <label>Para registrarse <a href='/registro'>Click Aquí</a></label>
-                            </div>
+                        </div>
+                        <div>
+                            <button id="BtnVerContrasena" type="button">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye-fill" viewBox="0 0 16 16">
+                                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                                    <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+                                </svg>
+                            </button>
+                            <label htmlFor="BtnVerContrasena">Mostrar Contraseña</label>
+                        </div>
+                        <button className="btn btn-primary" id="BtnIngresar" type='submit'>Ingresar</button>
+                        <div className="abajo">
+                            <label>Para registrarse <a href='/registro'>Click Aquí</a></label>
                         </div>
                     </div>
                 </div>
-            </form>
-        );
-    }
+            </div>
+        </form>
+    );
 }
-export default Login;
--- src/Paginas/Paciente/InicioPaciente.jsx
+++ src/Paginas/Paciente/InicioPaciente.jsx
@@ -3,64 +3,67 @@ import '../../Css-Componentes/Paciente/BackgroundPatient.css';
 import '../../Css-Componentes/Paciente/ListaDoctores.css';
 import '../../Imagenes/LogoDoctorFast1.png';
 
-class InicioPaciente extends Component{
-    render(){
-        return(
-            <div className="fondoSistema">
-                <div className="cuadro">
-                    <header className="headerPaciente">
-                        <img className="logo" src={require("../../Imagenes/LogoDoctorFast1.png")}/>
-                        <span className="nombreApp">DoctorFast</span>
-                    </header>
-                    <span className="menuIcono">☰</span>
-                    <div className="navigation">
-                        <ul className="ulNavegacion">
-                            <li className="liNavegacion">
-                                <a className="menuClickeado aNavegacion" href="/iniciopaciente"><i className="fa fa-home"></i><br/>Inicio</a>
-                            </li>
-                            <li>
-                                <a className="aNavegacion" href="/citaspaciente"><i className="fa fa-calendar-check-o"></i><br/>Citas</a>
-                            </li>
-                            <li>
-                                <a className="aNavegacion" href="/recetaspaciente"><i className="fa fa-file-text-o"></i><br/>Recetas</a>
-                            </li>
-                            <li>
-                                <a className="aNavegacion" href="/historialmedicopaciente"><i className="fa fa-paste"></i><br/>Historial Médico</a>
-                            </li>
-                        </ul>
+export default function InicioPaciente(){
+
+    let usuario = localStorage.getItem('user');
+
+    function getNombre(){
+        
+    }
+
+    return(
+        <div className="fondoSistema">
+            <div className="cuadro">
+                <header className="headerPaciente">
+                    <img className="logo" src={require("../../Imagenes/LogoDoctorFast1.png")}/>
+                    <span className="nombreApp">DoctorFast</span>
+                </header>
+                <span className="menuIcono">☰</span>
+                <div className="navigation">
+                    <ul className="ulNavegacion">
+                        <li className="liNavegacion">
+                            <a className="menuClickeado aNavegacion" href="/iniciopaciente"><i className="fa fa-home"></i><br/>Inicio</a>
+                        </li>
+                        <li>
+                            <a className="aNavegacion" href="/citaspaciente"><i className="fa fa-calendar-check-o"></i><br/>Citas</a>
+                        </li>
+                        <li>
+                            <a className="aNavegacion" href="/recetaspaciente"><i className="fa fa-file-text-o"></i><br/>Recetas</a>
+                        </li>
+                        <li>
+                            <a className="aNavegacion" href="/historialmedicopaciente"><i className="fa fa-paste"></i><br/>Historial Médico</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="contenido">
+                    <h1>Hola de nuevo Ricardo Haro...</h1>
+                    <div className="busqueda">
+                        <label>¿Qué tipo de especialidad estás buscando?</label>
+                        <input type="text"/>
+                        <button>Buscar</button>
                     </div>
-                    <div className="contenido">
-                        <h1>Hola de nuevo Ricardo Haro...</h1>
-                        <div className="busqueda">
-                            <label>¿Qué tipo de especialidad estás buscando?</label>
-                            <input type="text"/>
-                            <button>Buscar</button>
+                    <div className='Contenedor-Doctor'> 
+                        <div className='Contenedor-texto-Doctor'>
+                            <p className='Nombre-Doctor'>Julian Daniel Alvarez</p>
+                            <p className='Especialidad-Doctor'>Médico general</p>
+                            <p className='Horarios-Doctor'>11:00 a 16:00</p>
+                            <p className='Direccion-Doctor'>Av. 12 de Octubre y Colon</p>
+                            <p className='Calificacion'>🌟🌟🌟🌟🌟</p>
                         </div>
-                        <div className='Contenedor-Doctor'> 
-                            <div className='Contenedor-texto-Doctor'>
-                                <p className='Nombre-Doctor'>Julian Daniel Alvarez</p>
-                                <p className='Especialidad-Doctor'>Médico general</p>
-                                <p className='Horarios-Doctor'>11:00 a 16:00</p>
-                                <p className='Direccion-Doctor'>Av. 12 de Octubre y Colon</p>
-                                <p className='Calificacion'>🌟🌟🌟🌟🌟</p>
-                            </div>
-                            <button className='btn btn-outline-secondary BtnAgendar'>Agendar</button>
+                        <button className='btn btn-outline-secondary BtnAgendar'>Agendar</button>
+                    </div>
+                    <div className='Contenedor-Doctor'> 
+                        <div className='Contenedor-texto-Doctor'>
+                            <p className='Nombre-Doctor'>Gonzalo Vinicio Plata</p>
+                            <p className='Especialidad-Doctor'>Cardiología</p>
+                            <p className='Horarios-Doctor'>8:00 a 14:00</p>
+                            <p className='Direccion-Doctor'>Av. 6 de Diciembre y Río Coca</p>
+                            <p className='Calificacion'>🌟🌟🌟</p>
                         </div>
-                        <div className='Contenedor-Doctor'> 
-                            <div className='Contenedor-texto-Doctor'>
-                                <p className='Nombre-Doctor'>Gonzalo Vinicio Plata</p>
-                                <p className='Especialidad-Doctor'>Cardiología</p>
-                                <p className='Horarios-Doctor'>8:00 a 14:00</p>
-                                <p className='Direccion-Doctor'>Av. 6 de Diciembre y Río Coca</p>
-                                <p className='Calificacion'>🌟🌟🌟</p>
-                            </div>
-                            <button className='btn btn-outline-secondary BtnAgendar'>Agendar</button>
-                        </div>
+                        <button className='btn btn-outline-secondary BtnAgendar'>Agendar</button>
                     </div>
                 </div>
             </div>
-        );
-    };
-    
+        </div>
+    );
 }
-export default InicioPaciente;
