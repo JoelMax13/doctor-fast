3e63c039374355b025121c97a3165cd9fe17c0ed
JoelMax13 <joel.ona@hotmail.com>
2022-05-08 23:30:13 -0500

    DoctorFast React 0.5.3
    Intento obtención datos cliente

--- src/Paginas/Paciente/InicioPaciente.jsx
+++ src/Paginas/Paciente/InicioPaciente.jsx
@@ -5,20 +5,37 @@ import Saludo from "../../Components/Saludo";
 import axios from "axios";
 
 let uri = 'https://doctorfastapi.herokuapp.com/paciente';
+let usuario = localStorage.getItem('user');
 
 export default function InicioPaciente(){
 
-    let usuario = localStorage.getItem('user');
+
+    // var myHeaders = new Headers();
+    // myHeaders.append("token", usuario);
+    // var requestOptions = {
+    //     method: 'GET',
+    //     headers: myHeaders,
+    //     redirect: 'follow'
+    // };
 
     function getNombre(){
-        axios.get(uri, usuario)
-        .then(response => alert(response))
-        .catch(error => alert(error))
+        // usuario = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjE3NTEzNjkyMTQiLCJpYXQiOjE2NTIwNjcxNTUsImV4cCI6MTY1MjE1MzU1NX0.zkVNnyfCvNAo1W61z9OaJxV9QTN12OjMZyXve2saH0M";
+
+        var requestOptions = {
+            method: 'GET',
+            headers: {"token": usuario},
+            redirect: 'follow'
+        };
+
+        fetch(uri, requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
     }
 
     window.addEventListener('load', (event) => {
         getNombre();
-      });
+    });
 
     return(
         <div className="fondoSistema">
